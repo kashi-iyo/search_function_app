694f7281a23d13102f98ce26af52618512023bb0
kasi-iyo <snakeman143@gmail.com>
2020-05-29 17:22:25 +0900

    User, Post, PostFolderマイグレーションの作成

--- db/migrate/20200529080257_create_post_folders.rb
+++ db/migrate/20200529080257_create_post_folders.rb
@@ -1,9 +1,8 @@
 class CreatePostFolders < ActiveRecord::Migration[5.2]
   def change
     create_table :post_folders do |t|
-      t.string :folder_title
+      t.string :folder_title, null: false
       t.references :user, foreign_key: true
-      t.references :post, foreign_key: true
 
       t.timestamps
     end
