0097f63c9aa79e16c302d1510ff97685dded7bfe
kasi-iyo <snakeman143@gmail.com>
2020-05-30 11:20:08 +0900

    post_foldersコントローラ、postsコントローラへのルーティングを作成。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'posts#index'
+  root to: 'post_folders#index'
+  
+  resources :post_folders
+  get 'search', to: 'post_folders#search'
+
   resources :posts
-  get 'search', to: 'posts#search'
-  get 'select_search', to: 'posts#select_search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
